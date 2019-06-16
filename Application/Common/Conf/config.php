a75ca7254e72eb5b88af924f35d1c3f5900328d4
FKTV4SAF3QMVRXU\Administrator <57157865@qq.com>
2019-06-16 09:38:29 +0800

    修改密码

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,13 +1,13 @@
 <?php
 return [
-	// '配置项' => '配置值'
-	'DB_TYPE' 		          => 'mysql',
+    // '配置项' => '配置值'
+    'DB_TYPE' 		          => 'mysql',
     'DB_HOST' 		          => '127.0.0.1',
     'DB_USER' 		          => 'root',
-    'DB_PWD' 		          => 'root',
+    'DB_PWD' 		          => 'fzy1017463614',
     'DB_PORT' 		          => 3306,
     'DB_NAME' 		          => 'cms',
-    'DB_CHARSET' 	          => 'utf8',
+    'DB_CHARSET' 	          => 'utf8mb4',
     'DB_PREFIX' 	          => 'live_',
     'MD5_PREFIX'              => 'live99_',
     'LOAD_EXT_CONFIG'         =>'auth',
